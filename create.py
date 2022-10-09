7c86fc640745c885f5bbd16f78a4ceabde0990bd
kitamas <kitamas25@gmail.com>
2022-10-09 16:13:13 +0200

    commit

--- create.py
+++ create.py
@@ -65,51 +65,6 @@ def authentication():
     # Rounds to nearest hour by adding a timedelta hour if minute >= 30
     # return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour) + datetime.timedelta(hours=t.minute // 30))
 
-def check_open():
-    # current_dateTime = datetime.datetime.now() + datetime.timedelta(hours=3)
-    current_dateTime = datetime.datetime.now()
-    # 2022-10-07 16:03:58.003427
-
-    # hour_rounder !!! current_dateTime_rounded = hour_rounder(current_dateTime)
-    # 2022-10-07 15:00:00
-    current_dateTime_rounded = current_dateTime
-
-    hour_minute = current_dateTime_rounded.strftime('%H:%M')
-    print("HOUR:", hour_minute)
-
-    open_start_time = ["08:00", "08:00", "08:00", "08:00", "08:00", "08:00", "11:00"]
-    open_end_time = ["17:00", "17:00", "17:00", "17:00", "17:00", "13:00", "18:00"]
-
-    weekDays = ("hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat", "vasárnap")
-    week_day = current_dateTime.weekday()
-    # print("week day: ", week_day)
-
-    week_day_name = weekDays[week_day]
-    # print("week_day_name:", week_day_name)
-
-    # print("open_start_time:", open_start_time[week_day])
-    # print("open_end_time:", open_end_time[week_day])
-
-    # Textual month, day and year	
-    # d2 = current_dateTime.strftime("%Y %B, %d")
-    # 2022 October, 01
-
-    if hour_minute < open_start_time[week_day]:
-        # print("KORAN", hour_minute, "<", open_start_time[week_day])
-        text = "KORÁN. A mai nyitás " + open_start_time[week_day] + " a zárás " + open_end_time[week_day]
-
-    if hour_minute >= open_end_time[week_day]:
-        # print("KESON", hour_minute, ">=", open_end_time[week_day])
-        text = "KÉSŐN. A mai nyitás " + open_start_time[week_day] + " a zárás " + open_end_time[week_day]
-
-    if hour_minute >= open_start_time[week_day] and hour_minute <= open_end_time[week_day]:
-        # print("KOZOTTE", open_start_time[week_day], "<", hour_minute, "<", open_end_time[week_day])
-        text = "True"
-
-    print("CHECK OPEN RETURN TEXT",text)        
-    return text
-
-
 def webhook():
     # text = main()
     print("WEBHOOK 111")
@@ -222,4 +177,47 @@ def main():
     # return text
     return text_param
 
+def check_open():
+    # current_dateTime = datetime.datetime.now() + datetime.timedelta(hours=3)
+    current_dateTime = datetime.datetime.now()
+    # 2022-10-07 16:03:58.003427
+
+    # hour_rounder !!! current_dateTime_rounded = hour_rounder(current_dateTime)
+    # 2022-10-07 15:00:00
+    current_dateTime_rounded = current_dateTime
+
+    hour_minute = current_dateTime_rounded.strftime('%H:%M')
+    print("HOUR:", hour_minute)
+
+    open_start_time = ["08:00", "08:00", "08:00", "08:00", "08:00", "08:00", "11:00"]
+    open_end_time = ["17:00", "17:00", "17:00", "17:00", "17:00", "13:00", "18:00"]
+
+    weekDays = ("hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat", "vasárnap")
+    week_day = current_dateTime.weekday()
+    # print("week day: ", week_day)
+
+    week_day_name = weekDays[week_day]
+    # print("week_day_name:", week_day_name)
+
+    # print("open_start_time:", open_start_time[week_day])
+    # print("open_end_time:", open_end_time[week_day])
+
+    # Textual month, day and year	
+    # d2 = current_dateTime.strftime("%Y %B, %d")
+    # 2022 October, 01
+
+    if hour_minute < open_start_time[week_day]:
+        # print("KORAN", hour_minute, "<", open_start_time[week_day])
+        text = "KORÁN. A mai nyitás " + open_start_time[week_day] + " a zárás " + open_end_time[week_day]
+
+    if hour_minute >= open_end_time[week_day]:
+        # print("KESON", hour_minute, ">=", open_end_time[week_day])
+        text = "KÉSŐN. A mai nyitás " + open_start_time[week_day] + " a zárás " + open_end_time[week_day]
+
+    if hour_minute >= open_start_time[week_day] and hour_minute <= open_end_time[week_day]:
+        # print("KOZOTTE", open_start_time[week_day], "<", hour_minute, "<", open_end_time[week_day])
+        text = "True"
+
+    print("CHECK OPEN RETURN TEXT",text)        
+    return text
     app.run()
